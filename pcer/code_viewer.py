e7eac13c77e6ce35a9f2415cc3b8b0150cf56d55
Harrison Klein <harrisonklein123@gmail.com>
2019-07-10 15:34:19 -0400

    saves code viewer dimensions to config.txt, also changed highlight keywords from C++ to Java

--- pcer/code_viewer.py
+++ pcer/code_viewer.py
@@ -27,6 +27,11 @@ class CodeViewer(QWidget):
         self.listWidth = self.width/4
         self.backButtonWidth = self.listWidth/2
         self.initUI()
+        self.setFixedSize(800, 300)
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        config = open("config.txt", "a")
+        config.write(str(self.frameGeometry()) + "\n")
+        config.close()
 
     def initUI(self):
         self.setWindowFlags(QtCore.Qt.CustomizeWindowHint)
@@ -106,15 +111,20 @@ class Highlighter(QSyntaxHighlighter):
         keywordFormat.setForeground(Qt.darkBlue)
         keywordFormat.setFontWeight(QFont.Bold)
 
-        keywordPatterns = ["\\bchar\\b", "\\bclass\\b", "\\bconst\\b",
-                "\\bdouble\\b", "\\benum\\b", "\\bexplicit\\b", "\\bfriend\\b",
-                "\\binline\\b", "\\bint\\b", "\\blong\\b", "\\bnamespace\\b",
-                "\\boperator\\b", "\\bprivate\\b", "\\bprotected\\b",
-                "\\bpublic\\b", "\\bshort\\b", "\\bsignals\\b", "\\bsigned\\b",
-                "\\bslots\\b", "\\bstatic\\b", "\\bstruct\\b",
-                "\\btemplate\\b", "\\btypedef\\b", "\\btypename\\b",
-                "\\bunion\\b", "\\bunsigned\\b", "\\bvirtual\\b", "\\bvoid\\b",
-                "\\bvolatile\\b"]
+        keywordPatterns = ["\\babstract\\b", "\\bcontinue\\b", "\\bfor\\b",
+                "\\bnew\\b", "\\bswitch\\b", "\\bassert\\b", "\\bdefault\\b",
+                "\\bgoto\\b", "\\bpackage\\b", "\\bsynchronized\\b", "\\bboolean\\b",
+                "\\bdo\\b", "\\bif\\b", "\\bprivate\\b", "\\bthis\\b",
+                "\\bbreak\\b", "\\bdouble\\b", "\\bimplements\\b", "\\bprotected\\b",
+                "\\bthrow\\b", "\\bbyte\\b", "\\belse\\b", "\\bimport\\b",
+                "\\bpublic\\b", "\\bthrows\\b", "\\bcase\\b", "\\benum\\b",
+                "\\bintanceof\\b", "\\breturn\\b", "\\btransient\\b", "\\bcatch\\b",
+                "\\bextends\\b", "\\bint\\b", "\\bshort\\b", "\\btry\\b",
+                "\\bchar\\b", "\\bfinal\\b", "\\binterface\\b", "\\bstatic\\b",
+                "\\bvoid\\b", "\\bclass\\b", "\\bfinally\\b", "\\blong\\b",
+                "\\bstrictfp\\b", "\\bvolatile\\b", "\\bconst\\b", "\\bfloat\\b",
+                "\\bnative\\b", "\\bsuper\\b", "\\bwhile\\b", "\\btrue\\b"
+                "\\bfalse\\b", "\\bnull\\b"]
 
         self.highlightingRules = [(QRegExp(pattern), keywordFormat)
                 for pattern in keywordPatterns]
--- pcer/experiment_controller.py
+++ pcer/experiment_controller.py
@@ -7,16 +7,17 @@ class ExperimentController:
 
     def __init__(self):
         self.window = None
+        open("config.txt", "w").close
 
     def show_participant_form(self):
-    	if self.window is not None:
-    		self.window.close()
+        if self.window is not None:
+            self.window.close()
         self.window = ParticipantForm()
         self.window.submit_info.connect(self.show_system_form)
         self.window.show()
 
     def show_system_form(self):
-    	self.task_counter = 0
+        self.task_counter = 0
         self.window.close()
         self.window = SystemForm()
         self.window.back.connect(self.show_participant_form)

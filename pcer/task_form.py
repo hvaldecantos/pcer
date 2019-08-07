4d27e6c7f609f1bd732b78a000799f224dad3b2a
KaleChinmay <43867658+KaleChinmay@users.noreply.github.com>
2019-08-07 15:16:55 -0400

    Modified task_form.py for building task form

--- pcer/task_form.py
+++ pcer/task_form.py
@@ -3,11 +3,13 @@ from PyQt5.QtWidgets import (QWidget, QPushButton,
     QHBoxLayout, QVBoxLayout, QApplication)
 from PyQt5 import QtCore
 from pcer_window import PcerWindow
+from functools import partial
 
 class TaskForm(PcerWindow):
 
     submit_answer = QtCore.pyqtSignal()
     read_code = QtCore.pyqtSignal()
+    group_box = None
 
     def __init__(self, experiment):
         super(TaskForm, self).__init__(experiment)
@@ -18,6 +20,14 @@ class TaskForm(PcerWindow):
 
         submitButton.clicked.connect(self.onSubmitButtonClick)
         readButton.clicked.connect(self.onReadButtonClick)
+
+        task = self.experiment.getExperimentalTasks()
+        self.group_box, self.choice_combo_question_pair = self.experiment.form_builder.build_task_form(task)
+        self.vbox.addWidget(self.group_box)
+
+        for ccq_pair in self.choice_combo_question_pair:
+            cb = ccq_pair[1]
+            cb.currentIndexChanged.connect(partial(self.choiceSelection, ccq_pair))
 
         hbox = QHBoxLayout()
         hbox.addStretch(1)
@@ -31,9 +41,16 @@ class TaskForm(PcerWindow):
         self.setWindowTitle('Task presentation')
 
     def onSubmitButtonClick(self):
+        self.experiment.finishCurrentTask()
         print("TaskForm.onSubmitButtonClick")
         self.submit_answer.emit()
 
     def onReadButtonClick(self):
         print("TaskForm.onReadButtonClick")
         self.read_code.emit()
+
+    def choiceSelection(self,ccq_pair,i):
+        print('Choice :',ccq_pair[1].currentText(), ' is selected for question ',ccq_pair[0])
+        question = ccq_pair[0]
+        choice = ccq_pair[1].currentText()
+        self.experiment.setTaskData(question, choice)

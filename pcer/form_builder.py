557f20bea3e6348f637e30fed171ba6cc2d9d56d
hector <hvaldecantos@gmail.com>
2019-09-03 16:22:15 -0400

    adds font size for the form in the config file

--- pcer/form_builder.py
+++ pcer/form_builder.py
@@ -2,11 +2,30 @@ from PyQt5.QtWidgets import (QWidget, QPushButton,
     QHBoxLayout, QVBoxLayout, QApplication, QLabel, QComboBox, QGroupBox)
 from PyQt5 import QtCore
 from PyQt5.QtGui import QFont
+import yaml
 
 class FormBuilder:
 
     def __init__(self):
-        pass
+        config = yaml.load(open("config.yml"), Loader = yaml.SafeLoader)
+        self.font_type = config['form']['font']['type']
+        self.title_size = config['form']['font']['title_size']
+        self.title_bold = config['form']['font']['title_bold']
+        self.description_size = config['form']['font']['description_size']
+
+    def getTitleFont(self, bold = True):
+        font = QFont(self.font_type)
+        font.setFixedPitch(True)
+        font.setPixelSize(self.title_size)
+        font.setBold(self.title_bold and bold)
+        return font
+
+    def getDescriptionFont(self):
+        font = QFont(self.font_type)
+        font.setFixedPitch(True)
+        font.setPixelSize(self.description_size)
+        font.setBold(False)
+        return font
 
     def build_pretest_form(self, experiment):
         pretests = experiment.resource.getPretests(experiment.participant_group)
@@ -17,16 +36,20 @@ class FormBuilder:
         options = pretests['options']
 
         groupbox = QGroupBox(name)
+        groupbox.setFont(self.getTitleFont())
         inner_vbox = QVBoxLayout()
         inner_vbox.addStretch(1)
         choice_combo_question_list = []
 
         description_label = QLabel(description)
+        description_label.setFont(self.getDescriptionFont())
         for i in range(0,len(questions)):
             question_label = QLabel(questions[i])
+            question_label.setFont(self.getTitleFont(False))
             inner_vbox.addWidget(question_label)
             #question_widgets.append(question_label)
             choiceCombo = QComboBox()
+            choiceCombo.setFont(self.getDescriptionFont())
             for option in options:
                 choiceCombo.addItem(option)
             choice_combo_question_dict = {}
@@ -44,9 +67,10 @@ class FormBuilder:
         description = system['description']
         system_id = system['id']
         description_label = QLabel(description)
-        description_label.setFont(QFont('Verdana', 12))
+        description_label.setWordWrap(True)
+        description_label.setFont(self.getDescriptionFont())
         group_box = QGroupBox(name)
-        group_box.setFont(QFont('Arial', 16))
+        group_box.setFont(self.getTitleFont())
         inner_vbox = QVBoxLayout()
         inner_vbox.addWidget(description_label)
         group_box.setLayout(inner_vbox)
@@ -60,21 +84,23 @@ class FormBuilder:
         questions = task['questions']
         options = task['options']
         description_label = QLabel(description)
-        description_label.setFont(QFont('Verdana', 12))
+        description_label.setFont(self.getDescriptionFont())
         description_label.setWordWrap(True)
         group_box = QGroupBox(name)
-        group_box.setFont(QFont('Arial', 16))
+        group_box.setFont(self.getTitleFont())
         inner_vbox = QVBoxLayout()
         inner_vbox.addWidget(description_label)
+        inner_vbox.addWidget(QLabel(""))
         inner_vbox.addStretch(1)
         inner_vbox.setSpacing(10);
 
         for i in range(0,len(questions)):
             question_label = QLabel(questions[i])
-            question_label.setFont(QFont('Arial', 16))
+            question_label.setFont(self.getTitleFont(False))
+            question_label.setWordWrap(True)
             inner_vbox.addWidget(question_label)
             choiceCombo = QComboBox()
-            choiceCombo.setFont(QFont('Verdana', 12))
+            choiceCombo.setFont(self.getDescriptionFont())
             for option in options:
                 choiceCombo.addItem(option)
             choice_combo_question_dict = {}
